985dafbec2bdf9f550f0abab1c62991a3bd69a29
Andres Mendez <fmendez1212@gmail.com>
2022-09-10 20:25:17 -0500

    init

---
+++ CHPT1/CHPT1.cpp
@@ -0,0 +1,17 @@
+﻿#include "CHPT1.h"
+
+#include <fstream>
+#include <iostream>
+
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
